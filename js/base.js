f72dbc567217438cd4b8534be087722d269f0fa6
lixian13149999 <lixian13149999@163.com>
2016-06-21 18:23:16 +0800

    加入chart调试中

--- js/base.js
+++ js/base.js
@@ -1,13 +1,22 @@
 $(function(){
-    console.log('aa');
+//    左侧栏目点击后样式改变
     $(document).on("click",".menus .item-a",function(){
         var menusParent = $(this).parents(".menus").first();
-        console.log();
+//        console.log();
         menusParent.find(".items .item .item-a").each(function(index,ele){
 //            console.log(ele);
             $(ele).removeClass("checked-menu");
         });
 //        console.log($(menusParent))
         $(this).addClass("checked-menu");
+    })
+    
+//    分页点击后,样式改变
+    $(document).on("click",".pagination li .page-num",function(){
+        var pageParent = $(this).parents(".pagination").first();
+        pageParent.find("li .page-num").each(function(index,ele){
+            $(ele).removeClass("page-checked-a");
+        })
+        $(this).addClass("page-checked-a");
     })
 })
--- js/demo.js
+++ js/demo.js
@@ -19,6 +19,29 @@ $(function(){
 				console.log('加载页面出错');
 			}
 		});
-        console.log("aaaaaaaa");
+//        console.log("aaaaaaaa");
+    })
+
+    $(document).on("click","[data-refresh-page='achievement']",function(){
+        $.ajax({
+			url: './achievement.html',
+			type: 'GET',
+			data: {
+				// _id: user_id
+			},
+			async: false,
+			//- cache: false,
+			//- contentType: false,
+			//- processData: false,
+			success: function(data) {
+				 console.log(data);
+            $("#main").html(data);
+//				cb(data);
+			},
+			error: function() {
+				console.log('加载页面出错');
+			}
+		});
+//        console.log("aaaaaaaa");
     })
 })
