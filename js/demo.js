a81785a3e5c6e689ea4eede72dbc45a38cb6f992
lixian13149999 <lixian13149999@163.com>
2016-06-27 16:11:36 +0800

    页面重写基本完成

--- js/demo.js
+++ js/demo.js
@@ -20,11 +20,11 @@ $(function(){
 			}
 		});
 //        console.log("aaaaaaaa");
-    })
+    });
 
     $(document).on("click","[data-refresh-page='achievement']",function(){
         $.ajax({
-			url: './achi_gg.html',
+			url: './achi_fgs.html',
 			type: 'GET',
 			data: {
 				// _id: user_id
@@ -43,5 +43,74 @@ $(function(){
 			}
 		});
 //        console.log("aaaaaaaa");
-    })
+    });
+    
+    $(document).on("click","[data-refresh-page='sales']",function(){
+        $.ajax({
+			url: './sales.html',
+			type: 'GET',
+			data: {
+				// _id: user_id
+			},
+			async: false,
+			//- cache: false,
+			//- contentType: false,
+			//- processData: false,
+			success: function(data) {
+//				 console.log(data);
+            $("#main-container").html(data);
+//				cb(data);
+			},
+			error: function() {
+				console.log('加载页面出错');
+			}
+		});
+//        console.log("aaaaaaaa");
+    });
+
+    $(document).on("click","[data-refresh-item='sales']",function(){
+        $.ajax({
+			url: './sales_sales.html',
+			type: 'GET',
+			data: {
+				// _id: user_id
+			},
+			async: false,
+			//- cache: false,
+			//- contentType: false,
+			//- processData: false,
+			success: function(data) {
+//				 console.log(data);
+            $("#sales-sales-box").html(data);
+//				cb(data);
+			},
+			error: function() {
+				console.log('加载页面出错');
+			}
+		});
+//        console.log("aaaaaaaa");
+    });
+
+    $(document).on("click","[data-refresh-item='act']",function(){
+        $.ajax({
+			url: './sales_act.html',
+			type: 'GET',
+			data: {
+				// _id: user_id
+			},
+			async: false,
+			//- cache: false,
+			//- contentType: false,
+			//- processData: false,
+			success: function(data) {
+//				 console.log(data);
+            $("#sales-act-box").html(data);
+//				cb(data);
+			},
+			error: function() {
+				console.log('加载页面出错');
+			}
+		});
+//        console.log("aaaaaaaa");
+    });
 })
